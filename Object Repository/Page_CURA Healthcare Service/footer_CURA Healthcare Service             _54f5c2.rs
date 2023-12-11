a647764aedfa948cf8039769570a2c5d4ad7fbd0
1878 <1878@192.168.10.118>
2023-12-11 17:30:08 +0700

    next

---
+++ Object Repository/Page_CURA Healthcare Service/footer_CURA Healthcare Service             _54f5c2.rs
@@ -0,0 +1,149 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>footer_CURA Healthcare Service             _54f5c2</name>
+   <tag></tag>
+   <elementGuidId>ba50e5c5-5507-4e89-973c-b135de00b0b8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>footer</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//footer</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>footer</value>
+      <webElementGuid>cb57fd17-c2ab-4f7f-b633-ed34d755da63</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+    
+        
+            
+                CURA Healthcare Service
+                
+                Atlanta 550 Pharr Road NE Suite 525Atlanta, GA 30305
+                
+                     (678) 813-1KMS
+                     info@katalon.com
+                    
+                
+                
+                
+                    
+                        
+                    
+                    
+                        
+                    
+                    
+                        
+                    
+                
+                
+                Copyright © CURA Healthcare Service 2023
+            
+        
+    
+    
+</value>
+      <webElementGuid>71f3e722-a255-41b1-9925-0fc9f565e7ce</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/footer[1]</value>
+      <webElementGuid>c4024090-4657-4ba7-837a-67cd7ec6cac5</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//footer</value>
+      <webElementGuid>87c2824d-b653-4a8f-9baf-752bee8f1272</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//footer[(text() = '
+    
+        
+            
+                CURA Healthcare Service
+                
+                Atlanta 550 Pharr Road NE Suite 525Atlanta, GA 30305
+                
+                     (678) 813-1KMS
+                     info@katalon.com
+                    
+                
+                
+                
+                    
+                        
+                    
+                    
+                        
+                    
+                    
+                        
+                    
+                
+                
+                Copyright © CURA Healthcare Service 2023
+            
+        
+    
+    
+' or . = '
+    
+        
+            
+                CURA Healthcare Service
+                
+                Atlanta 550 Pharr Road NE Suite 525Atlanta, GA 30305
+                
+                     (678) 813-1KMS
+                     info@katalon.com
+                    
+                
+                
+                
+                    
+                        
+                    
+                    
+                        
+                    
+                    
+                        
+                    
+                
+                
+                Copyright © CURA Healthcare Service 2023
+            
+        
+    
+    
+')]</value>
+      <webElementGuid>6fbbba33-a450-429b-b492-b55e05e38831</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
